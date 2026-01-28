36f23654b2892b118647313c02329afdcac0dadc
type1mob <mobmob15476@gmail.com>
2026-01-28 20:13:30 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import "./styles/global.css";
 
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
